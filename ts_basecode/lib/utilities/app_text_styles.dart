9da8f8b25d28aabb3435fbd6ae73be53e730d67d
NinhTH <hninh170@gmail.com>
2024-08-09 11:45:21 +0700

    Create utilities folder

---
+++ ts_basecode/lib/utilities/app_text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  /// Splash Screen
+  static const appNameStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 24);
+}
---
+++ ts_basecode/lib/utilities/text_constants.dart
@@ -0,0 +1,15 @@
+class TextConstants {
+  TextConstants._();
+
+  /// Tab bar
+  static const calendar = "calendar";
+  static const map = "map";
+  static const weather = "weather";
+
+  /// Splash Screen
+  static const appName = "Training Schedule";
+
+  /// Onboarding Screen
+  static const next = 'Next';
+  static const started = 'Get started!';
+}
