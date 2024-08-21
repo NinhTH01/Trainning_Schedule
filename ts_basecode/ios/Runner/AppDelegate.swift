81e7ec7f879c2f2c4f738390baf2018528c56cf4
NinhTH <hninh170@gmail.com>
2024-08-21 16:19:25 +0700

    Remove native on repo

--- ts_basecode/ios/Runner/AppDelegate.swift
+++ ts_basecode/ios/Runner/AppDelegate.swift
@@ -5,51 +5,46 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
 
-      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-             GeneratedPluginRegistrant.register(with: registry)
-         }
-
-         if #available(iOS 10.0, *) {
-           UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-         }
-
-
-      UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-          if let error = error {
-              // Handle the error here.
-              print("Error requesting notification permissions: \(error.localizedDescription)")
-          }
-
-          if granted {
-              print("Notification permissions granted.")
-              // You can now register for remote notifications if needed.
-              DispatchQueue.main.async {
-                  UIApplication.shared.registerForRemoteNotifications()
-              }
-          } else {
-              print("Notification permissions denied.")
-          }
-      }
-
-      if let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
-         let keys = NSDictionary(contentsOfFile: path),
-         let apiKey = keys["googleMapsApiKey"] as? String {
-          GMSServices.provideAPIKey(apiKey)
+        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+            GeneratedPluginRegistrant.register(with: registry)
         }
 
-      guard let pluginRegisterar = self.registrar(forPlugin: "Runner") else { return false }
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+        }
 
-      let factory = FLNativeViewFactory(messenger: pluginRegisterar.messenger())
-      pluginRegisterar.register(
-          factory,
-          withId: "congratulation_view")
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                // Handle the error here.
+                print("Error requesting notification permissions: \(error.localizedDescription)")
+            }
+
+            if granted {
+                print("Notification permissions granted.")
+                // You can now register for remote notifications if needed.
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            } else {
+                print("Notification permissions denied.")
+            }
+        }
+
+        if let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+           let keys = NSDictionary(contentsOfFile: path),
+           let apiKey = keys["googleMapsApiKey"] as? String {
+            GMSServices.provideAPIKey(apiKey)
+        }
+
+        guard let pluginRegisterar = self.registrar(forPlugin: "Runner") else { return false }
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
